71f8a12786b87cdc0fcb8ec5e77428e664a64ae5
Brent Labasan <BrentLabasan@gmail.com>
2016-12-12 15:26:34 -0800

    left aligning banners; mobile

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -26,22 +26,37 @@ class Main extends React.Component {
 
   render() {
     return (
-      <div style={{textAlign: 'center'}}>
-        <a href="/portfolio/thriftbooks">
-        <img src='/thriftbooks/Thriftbooks-banner.jpg' alt='Thriftbooks banner'/>
-        </a>
-        <br/>
-        <a href="/portfolio/slalom-consulting">
-        <img src='/slalom-consulting/Slalom-Consulting-banner.jpg' alt='Slalom Consulting banner'/>
-        </a>
-        <br/>
-        <a href="/portfolio/intersect">
-        <img src='/intersect/Intersect-banner.jpg' alt='Intersect banner'/>
-        </a>
-        <br/>
-        <a href="/portfolio/metia">
-        <img src='/metia/Metia-banner.jpg' alt='Metia banner'/>
-        </a>
+      <div style={{ textAlign: 'center' }}>
+        <div style={{ overflow: 'hidden', backgroundUrl: '/thriftbooks/Thriftbooks-banner.jpg' }}>
+          <a href="/portfolio/thriftbooks">
+            <img src="/thriftbooks/Thriftbooks-banner.jpg" alt="Thriftbooks banner" />
+          </a>
+        </div>
+
+        <br />
+
+        <div style={{ overflow: 'hidden', backgroundUrl: '/slalom-consulting/Slalom-Consulting-banner.jpg' }}>
+          <a href="/portfolio/slalom-consulting">
+            <img src="/slalom-consulting/Slalom-Consulting-banner.jpg" alt="Slalom Consulting banner" />
+          </a>
+        </div>
+
+        <br />
+
+        <div style={{ overflow: 'hidden', backgroundUrl: '/intersect/Intersect-banner.jpg' }}>
+          <a href="/portfolio/intersect">
+            <img src="/intersect/Intersect-banner.jpg" alt="Intersect banner" />
+          </a>
+        </div>
+
+        <br />
+
+        <div style={{ overflow: 'hidden', backgroundUrl: '/metia/Metia-banner.jpg' }}>
+          <a href="/portfolio/metia">
+            <img src="/metia/Metia-banner.jpg" alt="Metia banner" />
+          </a>
+        </div>
+
       </div>
     );
   }
