697a847fe8ff452e545438216bac2cb0e487e87b
Brent Labasan <BrentLabasan@gmail.com>
2016-12-12 19:09:39 -0800

    SUCCESS layouts coming about

--- src/containers/ThriftBooks.js
+++ src/containers/ThriftBooks.js
@@ -9,7 +9,7 @@ class ThriftBooks extends React.Component {
 
         <Grid>
 
-          <Row className="show-grid">
+          {/* <Row className="show-grid">
             <Col xs={12} md={8}><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
             <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
           </Row>
@@ -27,14 +27,44 @@ class ThriftBooks extends React.Component {
           <Row className="show-grid">
             <Col md={6} mdPush={6}><code>&lt;{'Col md={6} mdPush={6}'} /&gt;</code></Col>
             <Col md={6} mdPull={6}><code>&lt;{'Col md={6} mdPull={6}'} /&gt;</code></Col>
-          </Row>
+          </Row>*/}
 
           <Row className="show-grid">
             <Col xs={12} className="noPadding"><img src="/thriftbooks/Thriftbooks-banner-lrg.jpg" alt="Thriftbooks banner" /></Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F3A326 txtAlignLeft"><h2>Developed ThriftBook's customer facing retail site.</h2></Col>
+            <Col xs={12} className="F3A326 txtSection txtAlignLeft">
+              <h2>Developed ThriftBook's customer facing retail site.</h2>
+            </Col>
+          </Row>
+
+          <Row className="show-grid">
+            <Col xs={12} className="noPadding"><img style={{ width: '100%' }} src="/thriftbooks/Thriftbooks-homepage.jpg" alt="Thriftbooks banner" /></Col>
+          </Row>
+
+          <Row className="show-grid">
+            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+              <h2>Updated and iterated on book pages’ edition selector interface according to customer suggestions.</h2>
+            </Col>
+          </Row>
+
+          <Row className="show-grid">
+            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+              <h2>Separated data for page loads into preliminary and React stages. For preliminary stage, only content necessary for SEO is rendered. React stage will then load additional content.</h2>
+            </Col>
+          </Row>
+
+          <Row className="show-grid">
+            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+              <h2>Created a React-based autocomplete component for product search field.</h2>
+            </Col>
+          </Row>
+
+          <Row className="show-grid">
+            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+              <h2>Used Redux and Flux for handling state interaction of various React components.</h2>
+            </Col>
           </Row>
 
         </Grid>
