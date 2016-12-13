5e1ee7eca6c5ae45f2511df804ebaac4745dacb2
Brent Labasan <BrentLabasan@gmail.com>
2016-12-12 18:23:13 -0800

    grid with banner kinda stable

--- src/containers/ThriftBooks.js
+++ src/containers/ThriftBooks.js
@@ -5,26 +5,34 @@ import { connect } from 'react-redux';
 class ThriftBooks extends React.Component {
   render() {
     return (
-      <div className="portfolioEntry ThriftBooks" style={{ textAlign: 'center' }}>
+      <div className="ThriftBooks" style={{ textAlign: 'center' }}>
 
-        {/* <div style={{ overflow: 'hidden' }}>
-          <img src="/thriftbooks/Thriftbooks-banner.jpg" alt="Thriftbooks banner" />
-        </div>
+        <Grid>
 
-        <div className="F3A326 txtSection txtAlignLeft height200">
-          <h2>Developed ThriftBook's customer facing retail site.</h2>
-        </div>*/}
-
-        <Grid fluid>
           <Row className="show-grid">
-            <Col xs={10} sm={12} md={12} lg={12}><img src="/thriftbooks/Thriftbooks-banner.jpg" alt="Thriftbooks banner" /></Col>
+            <Col xs={12} md={8}><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
+            <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
           </Row>
 
           <Row className="show-grid">
-
-            {/* <Col xs={10} className="F3A326"><h2>Developed ThriftBook's customer facing retail site.</h2></Col>*/}
+            <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+            <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+            <Col xsHidden md={4}><code>&lt;{'Col xsHidden md={4}'} /&gt;</code></Col>
           </Row>
 
+          <Row className="show-grid">
+            <Col xs={6} xsOffset={6}><code>&lt;{'Col xs={6} xsOffset={6}'} /&gt;</code></Col>
+          </Row>
+
+          <Row className="show-grid">
+            <Col md={6} mdPush={6}><code>&lt;{'Col md={6} mdPush={6}'} /&gt;</code></Col>
+            <Col md={6} mdPull={6}><code>&lt;{'Col md={6} mdPull={6}'} /&gt;</code></Col>
+          </Row>
+
+          <Row className="show-grid">
+            <Col xs={12} md={8}><img src="/thriftbooks/Thriftbooks-banner.jpg" alt="Thriftbooks banner" /></Col>
+            <Col xs={6} md={4}><code>&lt;{'Col xs={6} md={4}'} /&gt;</code></Col>
+          </Row>
 
         </Grid>
 
