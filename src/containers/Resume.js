924897c1e4074c212542b19c132b3deb7fb85079
Brent Labasan <BrentLabasan@gmail.com>
2017-01-29 07:49:24 -0800

    resume

--- src/containers/Resume.js
+++ src/containers/Resume.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { PageHeader } from 'react-bootstrap';
+import { Grid, Row, Col } from 'react-bootstrap';
+// import { Grid, Row, Col, ButtonGroup, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 
 class Resume extends React.Component {
   render() {
     return (
-      <div>
-        <PageHeader>Resume</PageHeader>
-        <p>Request a resume at <a href="mailto:BrentLabasan@gmail.com?Subject=Insert%Subject%20Here" target="_top">BrentLabasan@gmail.com</a>
-        </p>
+      <div className="portfolioEntry ThriftBooks" style={{ textAlign: 'center' }}>
+
+        <Grid>
+
+          <Row className="show-grid">
+
+            <Col xs={12} className="CF3D26 txtSection txtAlignLeft">
+              <Col xs={3}>
+                <h2>Resume</h2>
+              </Col>
+              <Col xs={9}>
+                <h4>Request a resume at <a href="mailto:BrentLabasan@gmail.com?Subject=Insert%Subject%20Here" target="_top">BrentLabasan@gmail.com</a></h4>
+              </Col>
+            </Col>
+
+          </Row>
+
+        </Grid>
+
       </div>
     );
   }
