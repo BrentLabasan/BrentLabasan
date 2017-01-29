a0aa4512c67af3143dc05699ed0481c3fbc60285
Brent Labasan <BrentLabasan@gmail.com>
2017-01-29 07:52:21 -0800

    Contact

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { PageHeader } from 'react-bootstrap';
+import { Grid, Row, Col } from 'react-bootstrap';
+// import { Grid, Row, Col, ButtonGroup, Button } from 'react-bootstrap';
 import { connect } from 'react-redux';
 
 class Contact extends React.Component {
   render() {
     return (
-      <div>
-        <PageHeader>Contact</PageHeader>
-        <p>Want to chat about web development jobs or consulting?</p>
-        <p>Send me a message at <a href="mailto:BrentLabasan@gmail.com?Subject=Insert%Subject%20Here" target="_top">BrentLabasan@gmail.com</a>
-        </p>
+
+      <div className="portfolioEntry ThriftBooks" style={{ textAlign: 'center' }}>
+
+        <Grid>
+
+          <Row className="show-grid">
+
+            <Col xs={12} className="CF3D26 txtSection txtAlignLeft">
+              <Col xs={3}>
+                <h2>Contact</h2>
+              </Col>
+              <Col xs={9}>
+                <h4>Want to chat about web development jobs or consulting?</h4>
+                <h4>Send me a message at <a href="mailto:BrentLabasan@gmail.com?Subject=Insert%Subject%20Here" target="_top">BrentLabasan@gmail.com</a></h4>
+              </Col>
+            </Col>
+
+          </Row>
+
+        </Grid>
+
       </div>
     );
   }
