8d7517095c49c57259ff4d98f684a78bd0dd3ca1
Brent Labasan <BrentLabasan@gmail.com>
2018-03-03 20:25:48 -0800

    CSS colors right

--- src/containers/MicrosoftAtlanta.js
+++ src/containers/MicrosoftAtlanta.js
@@ -6,7 +6,7 @@ import { connect } from 'react-redux';
 class MicrosoftAtlanta extends React.Component {
   render() {
     return (
-      <div className="portfolioEntry Microsoft_Atlanta" style={{ textAlign: 'center' }}>
+      <div className="portfolioEntry MicrosoftAtlanta" style={{ textAlign: 'center' }}>
 
         <Grid>
 
@@ -31,7 +31,7 @@ class MicrosoftAtlanta extends React.Component {
           </Row>*/}
 
           <Row className="show-grid">
-            <Col xs={12} className="noPadding"><img src="/microsoft-atlanta/Thriftbooks-banner-lrg.jpg" alt="Thriftbooks banner" /></Col>
+            <Col xs={12} className="noPadding"><img src="/microsoft-atlanta/Microsoft-Atlanta-banner-lrg.jpg" alt="Thriftbooks banner" /></Col>
           </Row>
 
           <br /><br />
@@ -44,7 +44,7 @@ class MicrosoftAtlanta extends React.Component {
           <br /><br /><br />
 
           <Row className="show-grid">
-            <Col xs={12} className="CF3D26 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-red txtSection txtAlignLeft">
               <h2>Developed the <a href="https://www.microsoft-atlanta.com" target="_blank">Microsoft_Atlanta</a> customer facing retail site. Front-end and back-end.</h2>
             </Col>
           </Row>
@@ -58,13 +58,13 @@ class MicrosoftAtlanta extends React.Component {
           <br />
 
           <Row className="show-grid">
-            <Col xs={12} className="F3A326 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-blue txtSection txtAlignLeft">
               <h3>Created a React-based autocomplete component for product search field.</h3>
             </Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87in">
+            <Col xs={12} className="ms-yellowin">
               {/* <video width="1460" height="822" controls>*/}
               <video width="90%" controls>
                 <source src="/microsoft-atlanta/Thriftbooks-autocomplete-basic-dummy-data.mp4" type="video/mp4" />
@@ -73,13 +73,13 @@ class MicrosoftAtlanta extends React.Component {
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-yellow txtSection txtAlignLeft">
               <h4>Initial version using dummy data.</h4>
             </Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87in">
+            <Col xs={12} className="ms-yellowin">
               {/* <video width="1460" height="822" controls>*/}
               <video width="90%" controls>
                 <source src="/microsoft-atlanta/Thriftbooks-autocomplete-basic-real-data-styled.mp4" type="video/mp4" />
@@ -88,13 +88,13 @@ class MicrosoftAtlanta extends React.Component {
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-yellow txtSection txtAlignLeft">
               <h4>Styled and using live data.</h4>
             </Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87in">
+            <Col xs={12} className="ms-yellowin">
               {/* <video width="1460" height="822" controls>*/}
               <video width="90%" controls>
                 <source src="/microsoft-atlanta/Thriftbooks-autocomplete-live.mp4" type="video/mp4" />
@@ -103,17 +103,17 @@ class MicrosoftAtlanta extends React.Component {
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-yellow txtSection txtAlignLeft">
               <h4>On live website.</h4>
             </Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87in"><img style={{ width: '100%' }} src="/microsoft-atlanta/Microsoft_Atlanta-traffic-autocomplete.png" alt="autocomplete traffic" /></Col>
+            <Col xs={12} className="ms-yellowin"><img style={{ width: '100%' }} src="/microsoft-atlanta/Microsoft_Atlanta-traffic-autocomplete.png" alt="autocomplete traffic" /></Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-yellow txtSection txtAlignLeft">
               <h4>Viewed by 2M users each week.</h4>
             </Col>
           </Row>
@@ -121,17 +121,17 @@ class MicrosoftAtlanta extends React.Component {
           <br />
 
           <Row className="show-grid">
-            <Col xs={12} className="F3A326 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-blue txtSection txtAlignLeft">
               <h3>Implemented a USPS address validation modal for shopping cart checkout process.</h3>
             </Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87in"><img style={{ width: '100%' }} src="/microsoft-atlanta/USPS-validation-modal.png" alt="USPS validation modal" /></Col>
+            <Col xs={12} className="ms-yellowin"><img style={{ width: '100%' }} src="/microsoft-atlanta/USPS-validation-modal.png" alt="USPS validation modal" /></Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-yellow txtSection txtAlignLeft">
               <h4>Used JavaScript, jQuery, and USPS’ API.
                 <br /><br />
                 Add some books to your cart on <a href="https://www.microsoft-atlanta.com" target="_blank">Microsoft_Atlanta</a> and navigate to the <a href="https://www.microsoft-atlanta.com/cart/checkout.aspx" target="_blank">shipping address</a> step of the checkout process.</h4>
@@ -139,31 +139,31 @@ class MicrosoftAtlanta extends React.Component {
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87in"><img style={{ width: '100%' }} src="/microsoft-atlanta/USPS-validation-modal-code.png" alt="USPS validation modal code" /></Col>
+            <Col xs={12} className="ms-yellowin"><img style={{ width: '100%' }} src="/microsoft-atlanta/USPS-validation-modal-code.png" alt="USPS validation modal code" /></Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-yellow txtSection txtAlignLeft">
               <h4>Some of the JavaScript code.</h4>
             </Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87in"><img style={{ width: '100%' }} src="/microsoft-atlanta/USPS-validation-modal-bug-fixing-cleanup.png" alt="USPS validation modal" /></Col>
+            <Col xs={12} className="ms-yellowin"><img style={{ width: '100%' }} src="/microsoft-atlanta/USPS-validation-modal-bug-fixing-cleanup.png" alt="USPS validation modal" /></Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-yellow txtSection txtAlignLeft">
               <h4>Code review.</h4>
             </Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87in"><img style={{ width: '100%' }} src="/microsoft-atlanta/Microsoft_Atlanta-traffic-USPS-modal.png" alt="USPS validation modal" /></Col>
+            <Col xs={12} className="ms-yellowin"><img style={{ width: '100%' }} src="/microsoft-atlanta/Microsoft_Atlanta-traffic-USPS-modal.png" alt="USPS validation modal" /></Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-yellow txtSection txtAlignLeft">
               <h4>Viewed by 50K users each week.</h4>
             </Col>
           </Row>
@@ -171,13 +171,13 @@ class MicrosoftAtlanta extends React.Component {
           <br />
 
           {/* <Row className="show-grid">
-            <Col xs={12} className="F3A326 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-blue txtSection txtAlignLeft">
               <h3>Separated data for page loads into preliminary and React stages.</h3>
             </Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87in">
+            <Col xs={12} className="ms-yellowin">
               <video width="90%" controls>
                 <source src="/microsoft-atlanta/Thriftbooks-preReact-React-stages.mp4" type="video/mp4" />
               </video>
@@ -185,7 +185,7 @@ class MicrosoftAtlanta extends React.Component {
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-yellow txtSection txtAlignLeft">
               <h4>For preliminary stage, only content necessary for SEO is rendered. React stage will then load additional content.</h4>
             </Col>
           </Row>
@@ -193,27 +193,27 @@ class MicrosoftAtlanta extends React.Component {
           <br /> */}
 
           {/* <Row className="show-grid">
-            <Col xs={12} className="F3A326 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-blue txtSection txtAlignLeft">
               <h3>Updated and iterated on product pages’ edition selector interface according to customer suggestions.</h3>
             </Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87in"><img style={{ width: '100%' }} src="/microsoft-atlanta/Thriftbooks-edition-selector-old-bw.jpg" alt="Thriftbooks banner" /></Col>
+            <Col xs={12} className="ms-yellowin"><img style={{ width: '100%' }} src="/microsoft-atlanta/Thriftbooks-edition-selector-old-bw.jpg" alt="Thriftbooks banner" /></Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-yellow txtSection txtAlignLeft">
               <h4>Customers were complaining that there wasn't enough information about the condition quality of used books.</h4>
             </Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87in"><img style={{ width: '100%' }} src="/microsoft-atlanta/Thriftbooks-edition-selector-new-bw.jpg" alt="Thriftbooks banner" /></Col>
+            <Col xs={12} className="ms-yellowin"><img style={{ width: '100%' }} src="/microsoft-atlanta/Thriftbooks-edition-selector-new-bw.jpg" alt="Thriftbooks banner" /></Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-yellow txtSection txtAlignLeft">
               <h4>Implented a redesigned interface to include condition quality status. Tweaked the tab button
               design to better indicate which tab is currently selected.</h4>
             </Col>
@@ -222,17 +222,17 @@ class MicrosoftAtlanta extends React.Component {
           <br /> */}
 
           <Row className="show-grid">
-            <Col xs={12} className="F3A326 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-blue txtSection txtAlignLeft">
               <h3>Published blog and landing pages written by our in-house content writers.</h3>
             </Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87in"><img style={{ width: '100%' }} src="/microsoft-atlanta/Thriftbooks-blog.jpg" alt="Thriftbooks blog entries" /></Col>
+            <Col xs={12} className="ms-yellowin"><img style={{ width: '100%' }} src="/microsoft-atlanta/Thriftbooks-blog.jpg" alt="Thriftbooks blog entries" /></Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-yellow txtSection txtAlignLeft">
               <h4>Wrote scripts to input page data into SQL Server, and then modified blog and landing page HTML templates to match specs.</h4>
             </Col>
           </Row>
@@ -240,13 +240,13 @@ class MicrosoftAtlanta extends React.Component {
           <br />
 
           <Row className="show-grid">
-            <Col xs={12} className="F3A326 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-blue txtSection txtAlignLeft">
               <h3>Designed and developed web apps for the admin area for our content writers and administrators: WordPress-style blog publishing tools (with TinyMCE), custom text on book product pages, etc...</h3>
             </Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87in">
+            <Col xs={12} className="ms-yellowin">
               {/* <video width="1460" height="822" controls>*/}
               <video width="90%" controls>
                 <source src="/microsoft-atlanta/Thriftbooks-admin-tool-marketing-text.mp4" type="video/mp4" />
@@ -255,7 +255,7 @@ class MicrosoftAtlanta extends React.Component {
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-yellow txtSection txtAlignLeft">
               <h4>Developed a feature to display custom marketing text on book products' pages. Text could be something like a custom review from one of ThriftBook's employees. You can see some of the code and
               dummy SQL data.
               </h4>
@@ -263,7 +263,7 @@ class MicrosoftAtlanta extends React.Component {
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87in">
+            <Col xs={12} className="ms-yellowin">
               {/* <video width="1460" height="822" controls>*/}
               <video width="90%" controls>
                 <source src="/microsoft-atlanta/Thriftbooks-admin-tool-marketing-text-interface.mp4" type="video/mp4" />
@@ -272,7 +272,7 @@ class MicrosoftAtlanta extends React.Component {
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-yellow txtSection txtAlignLeft">
               <h4>The behind-the-scenes admin area, for non-developers to write their own custom marketing text for products.
               </h4>
             </Col>
@@ -281,17 +281,17 @@ class MicrosoftAtlanta extends React.Component {
           <br />
 
           <Row className="show-grid">
-            <Col xs={12} className="F3A326 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-blue txtSection txtAlignLeft">
               <h3>Used Optimizely for A/B Testing.</h3>
             </Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87in"><img style={{ width: '100%' }} src="/microsoft-atlanta/Thriftbooks-Optimizely1.png" alt="Optimizely code and result" /></Col>
+            <Col xs={12} className="ms-yellowin"><img style={{ width: '100%' }} src="/microsoft-atlanta/Thriftbooks-Optimizely1.png" alt="Optimizely code and result" /></Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-yellow txtSection txtAlignLeft">
               <h4>jQuery was used for interface changes.</h4>
             </Col>
           </Row>
@@ -299,17 +299,17 @@ class MicrosoftAtlanta extends React.Component {
           <br />
 
           <Row className="show-grid">
-            <Col xs={12} className="F3A326 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-blue txtSection txtAlignLeft">
               <h3>Created and modified SQL tables, queries and stored procedures. In C# files, used either SQL query strings or LINQ to Entity.</h3>
             </Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87in"><img style={{ width: '100%' }} src="/microsoft-atlanta/Thriftbooks-LINQ.jpg" alt="LINQ queries" /></Col>
+            <Col xs={12} className="ms-yellowin"><img style={{ width: '100%' }} src="/microsoft-atlanta/Thriftbooks-LINQ.jpg" alt="LINQ queries" /></Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-yellow txtSection txtAlignLeft">
               <h4>LINQ queries in the marketing text controller. They will return a search query result, a paginated list, or a specific marketing text, respectively.</h4>
             </Col>
           </Row>
@@ -317,13 +317,13 @@ class MicrosoftAtlanta extends React.Component {
           <br />
 
           <Row className="show-grid">
-            <Col xs={12} className="F3A326 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-blue txtSection txtAlignLeft">
               <h3>Created and modified Mocha, Chai and Selenium tests for front end testing, and NUnit tests for backend unit testing. Used Browserstack and hallway testing for cross browser testing.</h3>
             </Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87in">
+            <Col xs={12} className="ms-yellowin">
               {/* <video width="1460" height="822" controls>*/}
               <video width="90%" controls>
                 <source src="/microsoft-atlanta/Thriftbooks-Selenium1.mp4" type="video/mp4" />
@@ -332,7 +332,7 @@ class MicrosoftAtlanta extends React.Component {
           </Row>
 
           <Row className="show-grid">
-            <Col xs={12} className="F7CF87 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-yellow txtSection txtAlignLeft">
               <h4>Running a Selenium test on my auto-complete.</h4>
             </Col>
           </Row>
@@ -340,7 +340,7 @@ class MicrosoftAtlanta extends React.Component {
           <br />
 
           {/* <Row className="show-grid">
-            <Col xs={12} className="F3A326 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-blue txtSection txtAlignLeft">
               <h3>Used Redux and Flux for handling state interaction of various React components.</h3>
             </Col>
           </Row>
@@ -348,7 +348,7 @@ class MicrosoftAtlanta extends React.Component {
           <br /> */}
 
           <Row className="show-grid">
-            <Col xs={12} className="CF3D26 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-red txtSection txtAlignLeft">
 
               <h2 name="BookClubHub">Developed <a href="https://BookClubHub.com" target="_blank">BookClubHub</a>, ThriftBook's social book club site.</h2>
 
@@ -364,7 +364,7 @@ class MicrosoftAtlanta extends React.Component {
           <br />
 
           <Row className="show-grid">
-            <Col xs={12} className="F3A326 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-blue txtSection txtAlignLeft">
               <h3>Created and maintained with HTML, SCSS, JavaScript, React, ASP.NET MVC 5, and SQL Server.</h3>
             </Col>
           </Row>
@@ -372,7 +372,7 @@ class MicrosoftAtlanta extends React.Component {
           <br />
 
           <Row className="show-grid">
-            <Col xs={12} className="F3A326 txtSection txtAlignLeft">
+            <Col xs={12} className="ms-blue txtSection txtAlignLeft">
               <h3>Used React to implement Clubs, Events, Invite RSVPs, and Profiles.</h3>
             </Col>
           </Row>
